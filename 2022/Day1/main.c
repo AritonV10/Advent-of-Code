d0e82c9867c4d8a04e5fa74cafd212805e7a173d
ganymede <ariton.vio@gmail.com>
2022-12-02 13:50:37 +0200

    Update main.c

--- 2022/Day1/main.c
+++ 2022/Day1/main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/types.h>
@@ -15,7 +7,6 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 static uint32_t
 MAIN__u32Stoi(const char *);
-
 
 int
 main(void)
