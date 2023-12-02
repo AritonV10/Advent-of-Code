b1a878b2439088970d14499fc7c8e2e2c20833d8
ganymede <ariton.vio@gmail.com>
2023-12-02 12:20:15 +0200

    Update main1.c

--- 2023/Day2/main1.c
+++ 2023/Day2/main1.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
@@ -14,9 +6,6 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #define nRed    12
 #define nGreen  13
 #define nBlue   14
-
-
-
 
 int main(void) {
   
